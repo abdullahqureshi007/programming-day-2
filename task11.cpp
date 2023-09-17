c38dd19dfd42e7b7dfdc75fb27c3a8a217559237
abdullahqureshi007 <qureshiabdullah1921@gmail.com>
2023-09-17 15:19:36 +0500

    message

---
+++ task11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+main() {
+    cout << "          /\\              " << endl;
+    cout << "         ____              " << endl;
+    cout << "       _/____\\_		" << endl;
+    cout << "      |________|		" << endl;
+    cout << "     |  ______  |		" << endl;
+    cout << "     | |      | |		" << endl;
+    cout << "     | |      | |		" << endl;
+    cout << "     | |______| |		" << endl;
+    cout << "     |  ______  |		" << endl;
+    cout << "     | |      | |		" << endl;
+    cout << "     | |      | |		" << endl;
+    cout << "     | |______| |		" << endl;
+    cout << "    /   ______   \\	" << endl;
+    cout << "   /   / |  | \\   \\	" << endl;
+    cout << "  /___/  \\  /  \\___\\	" << endl;
+    cout << "          \\/         	" << endl;
+
+
+}
---
+++ task13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "                                                        " << endl;
+    cout << "  +------------,______,--------------------,            " << endl;
+    cout << " \\\\|  |||||  ,_______,        |          (0           " << endl;
+    cout << "    ,'--------------------------1----------|            " << endl;
+    cout << "      '----------,---------,--------------'             " << endl;
+    cout << "      / XXXXX /\"\\\\     /                             " << endl;
+    cout << "     / XXXXX /   \\\\   /                               " << endl;
+    cout << "    / XXXXX /,---------,                                " << endl;
+    cout << "   / XXXXX /                                            " << endl;
+    cout << "  / XXXXX /                                             " << endl;
+    cout << " / XXXXX /                                              " << endl;
+    cout << "(________)                                              " << endl;
+
+    return 0;
+}
+
+
+
+
+
